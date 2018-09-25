38998aeba1be12d221465ccd78d6b1bf5b12b2fd
Riki Sholiardi <sholiardy@gmail.com>
2018-09-25 14:59:37 +0700

    add previews table

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 var express = require("express");
 var app = express();
 var multer = require("multer");
+var Excel = require("exceljs");
+var cors = require("cors");
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, contentType,Content-Type, Accept, Authorization");
-    next();
-});
+app.use(cors());
 
 var server = app.listen(process.env.PORT || 8080, function () {
     var port = server.address().port;
@@ -24,6 +21,28 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 app.post("/upload", upload.single('file'), (req, res) => {
-    console.log(req.file);
-    res.send(req.file.originalname);
+
+    var arrCol = []; 
+    var arrRow = [];
+
+    var workbook = new Excel.Workbook();
+    workbook.xlsx.readFile(req.file.path)
+        .then(function() {
+            workbook.eachSheet(function(worksheet, sheetId) {
+
+                var dobCol = worksheet.getColumn(1);
+                dobCol.eachCell(function(cell, rowNumber) {
+                    var row = worksheet.getRow(rowNumber);
+                    row.eachCell({ includeEmpty: true }, function(cell, colNumber) {
+                        arrRow.push(cell.value);
+                    });
+                    arrCol.push(arrRow);
+                    arrRow = [];
+               });
+
+               res.send(arrCol);
+
+            });
+        });
+
 });
